7bad9932fdb78a17eaf4fbe216defb005b37bc0c
ochwada <nakhulolinda@yahoo.com>
2025-06-13 12:47:51 +0200

    initial project setup

---
+++ src/main/java/com/library_book_management_system/App.java
@@ -0,0 +1,13 @@
+package com.library_book_management_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
